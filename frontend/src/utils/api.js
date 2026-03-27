ac15252fd5c7d1a17c813671f869f39775aa1692
bellaqiwang <bellaqiwang@tencent.com>
2026-03-27 22:47:51 +0800

    add base api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 // utils/api.js
-const API_BASE_URL = 'http://localhost:3000'
+const API_BASE_URL = 'http://localhost:3000/api'
 
 // Generic fetch wrapper with error handling
 const fetchApi = async (endpoint, options = {}) => {
